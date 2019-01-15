2046155d9e9fc7f8498141ae85c15c209a7de4ef
HEMEI-20180102W\Administrator <hanshouqing85@163.com>
2019-01-15 14:07:52 +0800

    控制台打印调试信息

--- lottery/proj.win32/main.cpp
+++ lottery/proj.win32/main.cpp
@@ -12,7 +12,20 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
+#if kGameLogEnabled
+	AllocConsole();
+	freopen("CONIN$", "r", stdin);
+	freopen("CONOUT$", "w", stdout);
+	freopen("CONOUT$", "w", stderr);
+#endif
+
     // create the application instance
     AppDelegate app;	
-	return Application::getInstance()->run();
+	int ret = Application::getInstance()->run();
+
+#if kGameLogEnabled
+	FreeConsole();
+#endif
+
+	return ret;
 }
